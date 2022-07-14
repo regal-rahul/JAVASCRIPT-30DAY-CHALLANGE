0b73a55827b23469558b4376400ab3a7a4f306fd
regal-rahul <regalrahul777@gmail.com>
2022-07-14 17:40:52 +0530

    volume and speed slider working

--- HTML Video Player/scripts.js
+++ HTML Video Player/scripts.js
@@ -20,8 +20,22 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
 /* Hook up the event listners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
