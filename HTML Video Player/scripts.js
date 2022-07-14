35f019b819c1c7a3fb7bf38f8a62ad99d42b03a6
regal-rahul <regalrahul777@gmail.com>
2022-07-14 18:51:04 +0530

    added fullscreen and picture in picture mode

--- HTML Video Player/scripts.js
+++ HTML Video Player/scripts.js
@@ -37,7 +37,19 @@ function scrub(e) {
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
 }
+var elem = document.getElementById("myvideo");
 
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
 
 
 /* Hook up the event listners */
