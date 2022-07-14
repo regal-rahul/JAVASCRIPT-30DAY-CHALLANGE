75080d84a125d4304639d95c99dc3af50ca87b38
regal-rahul <regalrahul777@gmail.com>
2022-07-14 17:26:07 +0530

    video play and pause method

--- HTML Video Player/scripts.js
+++ HTML Video Player/scripts.js
@@ -4,11 +4,24 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('.[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 
 /* Build out Functions */
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
 
+
+function updateButton() {
+    const icon = this.pause ? '►' : '❚ ❚';
+    console.log('Update the button');
+}
 
 /* Hook up the event listners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
